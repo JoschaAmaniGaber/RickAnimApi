84688d32b77e09aa536bf95f7cb37bd4d82a7703
joschaamanigaber <joscha.amani.gaber@gmail.com>
2024-06-21 03:48:44 +0200

    Animation & Strings

--- app/src/main/java/de/example/ricksanimationapi/MainActivity.kt
+++ app/src/main/java/de/example/ricksanimationapi/MainActivity.kt
@@ -1,20 +1,22 @@
 package de.example.ricksanimationapi
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import de.example.ricksanimationapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
+        setContentView(binding.root)
+
+        val bg_anim_main: AnimationDrawable = binding.root.background as AnimationDrawable
+        bg_anim_main.setEnterFadeDuration(8)
+        bg_anim_main.setExitFadeDuration(8888)
+        bg_anim_main.start()
     }
+
 }
