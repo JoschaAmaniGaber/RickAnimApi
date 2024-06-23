043795dee758d3741f9a59148a83191eb02e008f
joschaamanigaber <joscha.amani.gaber@gmail.com>
2024-06-23 17:07:27 +0200

    Build Screens for choose between RickApiApp and Introducing Myself

--- app/src/main/java/de/example/ricksanimationapi/MainActivity.kt
+++ app/src/main/java/de/example/ricksanimationapi/MainActivity.kt
@@ -2,7 +2,9 @@ package de.example.ricksanimationapi
 
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import de.example.ricksanimationapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +15,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater, null, false)
         setContentView(binding.root)
 
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                binding.fragmentContainerView.findNavController().navigateUp()
+            }
+        })
+
         val bg_anim_main: AnimationDrawable = binding.root.background as AnimationDrawable
         bg_anim_main.setEnterFadeDuration(8)
-        bg_anim_main.setExitFadeDuration(8888)
+        bg_anim_main.setExitFadeDuration(2222)
         bg_anim_main.start()
     }
 
--- app/src/main/java/de/example/ricksanimationapi/ui/AmaniDuniaFragment.kt
+++ app/src/main/java/de/example/ricksanimationapi/ui/AmaniDuniaFragment.kt
@@ -6,12 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import de.example.ricksanimationapi.databinding.FragmentWelcomeBinding
+import de.example.ricksanimationapi.databinding.FragmentAmaniDuniaBinding
 
-class WelcomeFragment : Fragment() {
+class AmaniDuniaFragment : Fragment() {
 
-    private lateinit var binding: FragmentWelcomeBinding
+    private lateinit var binding: FragmentAmaniDuniaBinding
     // TODO private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -19,7 +18,7 @@ class WelcomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentWelcomeBinding.inflate(layoutInflater)
+        binding = FragmentAmaniDuniaBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -27,27 +26,15 @@ class WelcomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         startAnimBtBg()
-
-        binding.apply {
-
-            clWelcomeRick.setOnClickListener {
-
-            }
-
-            clWelcomeAmani.setOnClickListener {
-                val navController = Navigation.findNavController(root)
-                val directions = WelcomeFragmentDirections.toAmaniDuniaFragment()
-                navController.navigate(directions)
-            }
-        }
     }
 
     private fun startAnimBtBg() {
 
-        val animRick: AnimationDrawable = binding.clWelcomeRick.background as AnimationDrawable
-        val animAmani: AnimationDrawable = binding.clWelcomeAmani.background as AnimationDrawable
+        val anim1: AnimationDrawable = binding.cl.background as AnimationDrawable
+        val anim2: AnimationDrawable = binding.constraintLayout.background as AnimationDrawable
+        val animHeader: AnimationDrawable = binding.clAmaniHeader.background as AnimationDrawable
 
-        val animWelcomeList: List<AnimationDrawable> = listOf(animAmani, animRick)
+        val animWelcomeList: List<AnimationDrawable> = listOf(animHeader, anim2, anim1)
         var anim: AnimationDrawable
 
         repeat(animWelcomeList.size) {
