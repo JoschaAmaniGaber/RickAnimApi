da223d6dfc0fa129a7167dd423a488e1335d8fc7
joschaamanigaber <joscha.amani.gaber@gmail.com>
2024-06-24 03:17:34 +0200

    Add Animations and clean up Animation Code Add Anims.kt for faster setting up Animations and Set them Off

---
+++ app/src/main/java/de/example/ricksanimationapi/ui/RickDbCheckFragment.kt
@@ -0,0 +1,26 @@
+package de.example.ricksanimationapi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import de.example.ricksanimationapi.MainViewModel
+import de.example.ricksanimationapi.databinding.FragmentRickDbCheckBinding
+
+class RickDbCheckFragment : Fragment() {
+
+    private lateinit var binding: FragmentRickDbCheckBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRickDbCheckBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
